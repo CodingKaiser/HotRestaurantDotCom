eb5a4cb57c4a3f91e09ef5543a83b51a40b14079
CodingKaiser <falkonoe@live.com>
2017-04-22 11:37:24 -0700

    Finished rough backend post and get.

---
+++ app/routing/routes.js
@@ -0,0 +1,30 @@
+var path = require("path");
+
+var waitlist = [];
+var reservations = [];
+
+module.exports = function setupRoutes(app, express) {
+  console.log(__dirname);
+  app.use(express.static('../public'));
+
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitlist);
+  });
+
+  app.get("/api/reservations", function(req, res) {
+    return res.json(reservations);
+  });
+
+  app.post("/api/new", (req,res) => {
+    var newReservation = req.body;
+    if (reservations.length < 5){
+      reservations.push(newReservation);
+    } else {
+      console.log("You are on a waitlist");
+      waitlist.push(newReservation);
+    }
+    return res.json(newReservation);
+  });
+
+
+};
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var setupRoutes = require("./app/routing/routes.js");
 
 var app = express();
 var PORT = 3000;
@@ -10,3 +11,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+app.use(express.static(path.join(__dirname, 'app/public')));
+
+setupRoutes(app, express);
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
