9e73cb6080ee54674289957496699a4050873a5a
CodingKaiser <falkonoe@live.com>
2017-04-22 10:47:23 -0700

    Merged the two files into one called routes.js

--- app/routing/htmlRoutes.js
+++
@@ -1,8 +0,0 @@
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "view.html"));
-});
-
-app.get("/survey", function(req, res) {
-  res.sendFile(path.join(__dirname, "add.html"));
-});
